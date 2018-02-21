a16346b82df9a93d463311f46992ecffdbbe3828
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-02-21 19:02:07 +0100

    fix: add git extension to url

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# if travis environment go to build dir
-if [ -n "$TRAVIS_BUILD_DIR" ]; then
-  cd $TRAVIS_BUILD_DIR
-fi
-
 # configure git name and email
 git config --global user.name "Travis CI"
 git config --global user.email "ivandelabeldad@gmail.com"
@@ -12,7 +7,7 @@ git config --global user.email "ivandelabeldad@gmail.com"
 # add git repository and pull all branches
 mkdir out && cd out
 git init
-git remote add origin https://$GITHUB_TOKEN@github.com/ivandelabeldad/rackian-cloud
+git remote add origin https://ivandelabeldad:$GITHUB_TOKEN@github.com/ivandelabeldad/rackian-cloud.git
 git pull --all
 git checkout master
 
